0ab88d3127eb0b911805aef3458ba529bdaab668
= <=>
2020-03-09 17:28:44 -0700

    header v1

---
+++ cyn_header.js
@@ -0,0 +1,9 @@
+var close_State = true;
+
+function menuOpen(){
+    document.querySelector(".cyn_nav").style.opacity = 100;
+}
+
+function menuClose(){
+    document.querySelector(".cyn_nav").style.opacity = 0;
+}
