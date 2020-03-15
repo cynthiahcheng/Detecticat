434816789dd3765d579bb6a3afc7d86443ad68be
= <=>
2020-03-14 23:21:02 -0700

    linked pages

--- .history/contact page/contact-us_20200314203236.js
+++
@@ -1,9 +0,0 @@
-var close_State = true;
-
-function menuOpen(){
-    document.querySelector(".nav").style.opacity = 100;
-}
-
-function menuClose(){
-    document.querySelector(".nav").style.opacity = 0;
-}
